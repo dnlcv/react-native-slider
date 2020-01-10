a5b91c47a2623e7ea88d0efefd3959d6b7971a6a
Daniel Campos <daniel.campos@bodybuilding.com>
2020-01-10 05:14:44 -0600

    Use UNSAFE_

--- src/Slider.js
+++ src/Slider.js
@@ -196,7 +196,7 @@ export default class Slider extends PureComponent {
     value: new Animated.Value(this.props.value),
   };
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: this._handleStartShouldSetPanResponder,
       onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder,
@@ -208,7 +208,7 @@ export default class Slider extends PureComponent {
     });
   };
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     var newValue = nextProps.value;
 
     if (this.props.value !== newValue) {
